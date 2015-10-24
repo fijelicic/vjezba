f6be486c3284ec14cc03fece5a8c63c57be79aa0
fijelicic <fijelicic@gmail.com>
2015-10-24 15:04:25 +0200

    first commit

---
+++ app/src/main/java/com/example/filip/beerlocator/FindBeer.java
@@ -0,0 +1,13 @@
+package com.example.filip.beerlocator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindBeer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_beer);
+    }
+}
